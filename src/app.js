bbb4df437df007e7fc8635ce843bd6bc2e4c6d5a
SKYjithyapa <125755221+SKYjithyapa@users.noreply.github.com>
2023-11-02 18:57:47 +0530

    third

--- src/app.js
+++ src/app.js
@@ -8,17 +8,18 @@ const BuyerRoutes = require("./routes/buyer");
 const CompanyRoutes = require("./routes/company");
 const ProductRoutes = require("./routes/products")
 const ProductController = require("./routes/products")
-const fileUpload  = require("express-fileupload");
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+
 app.use('uploads/products',express.static("public/products"))
 
 
-app.use(fileUpload( ));
 
 
 app.use(express.json());
 app.use("/api/v1/buyers",BuyerRoutes);
 app.use("/api/v1/companies",CompanyRoutes);
-app.use("/api/v1/products",Pr)
+app.use("/api/v1/products",ProductController)
 
 app.use((err, req, res, next) => {
   if (createHttpError.isHttpError(err)) {
